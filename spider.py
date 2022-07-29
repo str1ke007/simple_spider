2d9810bfbe2dec81d80b6d0e15bf384f19107e96
str1ke007 <73118143+str1ke007@users.noreply.github.com>
2022-07-29 19:55:28 +0530

    Update spider.py

--- spider.py
+++ spider.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from requests.exceptions import ConnectionError, HTTPError
+from requests.exceptions import ConnectionError, HTTPError, InvalidURL
 from urllib3.exceptions import NewConnectionError, LocationParseError
 import requests
 import re
@@ -17,7 +17,7 @@ def get_links_from():
 			flag = 1
 			response = requests.get(f"https://{url}/", timeout=2)
 			response.raise_for_status()
-		except (ConnectionError, NewConnectionError, LocationParseError, HTTPError):
+		except (ConnectionError, NewConnectionError, LocationParseError, HTTPError, InvalidURL):
 			print(f"\n{c.RED}[-] URL invalid!\n[-] Please wait...")
 			flag = 0
 			sleep(2)
