d750f46ea1073516d4fc8e42dd6538775718ec40
str1ke007 <73118143+str1ke007@users.noreply.github.com>
2022-07-29 19:37:34 +0530

    minor change
    error handling (only with Urls, not for IP)

--- spider.py
+++ spider.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python3
 
-import requests as r
+from requests.exceptions import ConnectionError, HTTPError
+from urllib3.exceptions import NewConnectionError
+import requests
 import re
 import colors as c
 from time import sleep
+import os
 
 
-def get_links_from(url):
-	try:
-		response = r.get(f"https://{url}/", timeout=2)
-		if response.status_code == 200:
-			return re.findall('(?:href=")(.*?)"', str(response.content))
-		else:
-			print(f"\n\n{c.RED}[-] URL invalid!\n\n")
-			exit(0)
-	except EXCEPTION:
-		print(f"\n\n{c.RED}[-] Error!\n\n")
+def get_links_from():
+	flag = 0
+	while True:
+		try:
+			url = input(f"{c.GREEN}[+] Enter URL{c.RED}(only!){c.GREEN}: ")
+			flag = 1
+			response = requests.get(f"https://{url}/", timeout=2)
+			response.raise_for_status()
+		except (ConnectionError, NewConnectionError, HTTPError):
+			print(f"\n{c.RED}[-] URL invalid!\n[-] Please wait...")
+			flag = 0
+			sleep(2)
+			os.system('clear')
+		if flag == 1:
+			break
+
+	return re.findall('(?:href=")(.*?)"', str(response.content))
 
 
 def main():
 	url_list = []
-	url_result = get_links_from(url)
+	url_result = get_links_from()
 	print()
 	for ur in url_result:
 		url_f = re.findall('(?:.*://)(.*)', str(ur))
@@ -31,21 +41,20 @@ def main():
 def display(url_list):
 	count = 0
 	url_result = []
-	print(f"\n{c.GREEN}[-] URL: {url}\n")
 	for x in url_list:
 		if x:
 			for y in x:
 				url_result.append(y)
 				print(f"{c.MAGENTA}[-] {y}")
 				count += 1
-	print(f"\n\n{c.RED}[-] Total number of links: {count}\n\n")
-	print(f"{c.GREEN}[-] Saving links in a {c.RED}.txt {c.GREEN}file\n")
+	print(f"\n\n{c.RED}[+] Total number of links: {count}\n\n")
+	print(f"{c.GREEN}[+] Saving links in a {c.RED}.txt {c.GREEN}file\n")
 	save_in_disk(url_result)
 	if count > 50:
 		sleep(10)
 	else:
 		sleep(5)
-	print(f"{c.GREEN}[-] Links stored successfully in {c.RED}url.txt\n\n{c.WHITE}")
+	print(f"{c.GREEN}[+] Links stored successfully in {c.RED}{os.getcwd()}/url.txt\n\n{c.WHITE}")
 
 
 def save_in_disk(url_result):
@@ -54,5 +63,4 @@ def save_in_disk(url_result):
 		   		f.write(f"{x}\n")
 
 
-url = input(f"{c.GREEN}[-] Enter URL: ")
 main()
